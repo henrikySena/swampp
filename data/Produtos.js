9d43c6bd23f8c227559409cf3633bd69564c1e89
henrikySena <henriky.sena55@gmail.com>
2025-05-09 11:49:40 -0300

    Organização do banco de dados

--- data/Produtos.js
+++ data/Produtos.js
@@ -1,4 +1,271 @@
+// produtos em destque (renderizado na home) 
 export const produtosEmDestaque = [
+  {
+    id: 9,
+    nome: "Chápeu de Bebê Natação Anti-UV Reversível",
+    marca: "Nabaiji",
+    preco: 54.99,
+    categoria: "acessorio",
+    imagens: [
+      "/produtos/acessorios/chapeus/chapeu-de-bebe-natacao-anti-uv-reversivel-nabaiji/chapeu-de-bebe-natacao-anti-uv-reversivel-nabaiji.jpg",
+      "/produtos/acessorios/chapeus/chapeu-de-bebe-natacao-anti-uv-reversivel-nabaiji/chapeu-de-bebe-natacao-anti-uv-reversivel-nabaiji1.jpg",
+      "/produtos/acessorios/chapeus/chapeu-de-bebe-natacao-anti-uv-reversivel-nabaiji/chapeu-de-bebe-natacao-anti-uv-reversivel-nabaiji2.jpg",
+      "/produtos/acessorios/chapeus/chapeu-de-bebe-natacao-anti-uv-reversivel-nabaiji/chapeu-de-bebe-natacao-anti-uv-reversivel-nabaiji3.jpg"
+    ],
+    variacoes: []
+  },
+  {
+    id: 23, 
+    nome: "Coleira para Cão Outdoor 500",
+    marca: "Solognac",
+    preco: 89.99,
+    categoria: "equipamento",
+    imagens: [],
+    variacoes: [
+      {
+        cor: "Vermelho",
+        corHex: "#983728",
+        imagens: [
+          "/produtos/equipamentos/outros/coleira-para-cão-outdoor-500-solognac/vermelho/coleira-para-cão-outdoor-500-solognac-vermelho.jpg",
+          "/produtos/equipamentos/outros/coleira-para-cão-outdoor-500-solognac/vermelho/coleira-para-cão-outdoor-500-solognac-vermelho1.jpg",
+          "/produtos/equipamentos/outros/coleira-para-cão-outdoor-500-solognac/vermelho/coleira-para-cão-outdoor-500-solognac-vermelho2.jpg",
+          "/produtos/equipamentos/outros/coleira-para-cão-outdoor-500-solognac/vermelho/coleira-para-cão-outdoor-500-solognac-vermelho3.jpg",
+
+        ]
+      },
+      {
+        cor: "Amarelo",
+        corHex: "#D4AA48",
+        imagens: [
+          "/produtos/equipamentos/outros/coleira-para-cão-outdoor-500-solognac/amarelo/coleira-para-cão-outdoor-500-solognac-amarelo.jpg",
+          "/produtos/equipamentos/outros/coleira-para-cão-outdoor-500-solognac/amarelo/coleira-para-cão-outdoor-500-solognac-amarelo1.jpg",
+          "/produtos/equipamentos/outros/coleira-para-cão-outdoor-500-solognac/amarelo/coleira-para-cão-outdoor-500-solognac-amarelo2.jpg",
+          "/produtos/equipamentos/outros/coleira-para-cão-outdoor-500-solognac/amarelo/coleira-para-cão-outdoor-500-solognac-amarelo3.jpg",
+        ]
+      },
+      {
+        cor: "Azul",
+        corHex: "#1A92AD",
+        imagens: [
+          "/produtos/equipamentos/outros/coleira-para-cão-outdoor-500-solognac/azul/coleira-para-cão-outdoor-500-solognac-azul.jpg",
+          "/produtos/equipamentos/outros/coleira-para-cão-outdoor-500-solognac/azul/coleira-para-cão-outdoor-500-solognac-azul1.jpg",
+          "/produtos/equipamentos/outros/coleira-para-cão-outdoor-500-solognac/azul/coleira-para-cão-outdoor-500-solognac-azul2.jpg",
+          "/produtos/equipamentos/outros/coleira-para-cão-outdoor-500-solognac/azul/coleira-para-cão-outdoor-500-solognac-azul3.jpg",
+        ]
+      }
+    ]
+  },
+  {
+    id: 21,
+    nome: "Cadeira baixa compacta 500 para camping",
+    marca: "Quechua",
+    preco: 349.99,
+    categoria: "equipamento",
+    imagens: [],
+    variacoes: [
+      {
+        cor: "Bege",
+        corHex: "#AFACA1",
+        imagens: [
+          "/produtos/equipamentos/cadeiras/cadeira-baixa-compacta-500-para-camping-quechua/bege/cadeira-baixa-compacta-500-para-camping-bege.jpg",
+          "/produtos/equipamentos/cadeiras/cadeira-baixa-compacta-500-para-camping-quechua/bege/cadeira-baixa-compacta-500-para-camping-bege1.jpg",
+          "/produtos/equipamentos/cadeiras/cadeira-baixa-compacta-500-para-camping-quechua/bege/cadeira-baixa-compacta-500-para-camping-bege2.jpg",
+          "/produtos/equipamentos/cadeiras/cadeira-baixa-compacta-500-para-camping-quechua/bege/cadeira-baixa-compacta-500-para-camping-bege3.jpg",
+          "/produtos/equipamentos/cadeiras/cadeira-baixa-compacta-500-para-camping-quechua/bege/cadeira-baixa-compacta-500-para-camping-bege4.jpg"
+        ]
+      },
+      {
+        cor: "Verde",
+        corHex: "#445353",
+        imagens: [
+          "/produtos/equipamentos/cadeiras/cadeira-baixa-compacta-500-para-camping-quechua/verde/cadeira-baixa-compacta-500-para-camping-verde.jpg",
+          "/produtos/equipamentos/cadeiras/cadeira-baixa-compacta-500-para-camping-quechua/verde/cadeira-baixa-compacta-500-para-camping-verde1.jpg",
+          "/produtos/equipamentos/cadeiras/cadeira-baixa-compacta-500-para-camping-quechua/verde/cadeira-baixa-compacta-500-para-camping-verde2.jpg",
+          "/produtos/equipamentos/cadeiras/cadeira-baixa-compacta-500-para-camping-quechua/verde/cadeira-baixa-compacta-500-para-camping-verde3.jpg",
+          "/produtos/equipamentos/cadeiras/cadeira-baixa-compacta-500-para-camping-quechua/verde/cadeira-baixa-compacta-500-para-camping-verde4.jpg"
+        ]
+      }
+    ]
+  },
+  {
+    id: 5,
+    nome: "Camiseta Masculina de Pilates Essentials 500",
+    marca: "Domyos",
+    preco: 69.99,
+    categoria: "roupa",
+    imagens: [],
+    variacoes: [
+      {
+        cor: "Rosa",
+        corHex: "#E4758A",
+        imagens: [
+          "/produtos/roupas/camiseta-masculina-de-pilates-essentials-500-domyos/rosa/camiseta-masculina-de-pilates-essentials-500-domyos-rosa.jpg",
+          "/produtos/roupas/camiseta-masculina-de-pilates-essentials-500-domyos/rosa/camiseta-masculina-de-pilates-essentials-500-domyos-rosa1.jpg",
+          "/produtos/roupas/camiseta-masculina-de-pilates-essentials-500-domyos/rosa/camiseta-masculina-de-pilates-essentials-500-domyos-rosa2.jpg",
+          "/produtos/roupas/camiseta-masculina-de-pilates-essentials-500-domyos/rosa/camiseta-masculina-de-pilates-essentials-500-domyos-rosa3.jpg"
+        ]
+      },
+      {
+        cor: "Amarela",
+        corHex: "#FEEC95",
+        imagens: [
+          "/produtos/roupas/camiseta-masculina-de-pilates-essentials-500-domyos/amarela/camiseta-masculina-de-pilates-essentials-500-domyos-amarela.jpg",
+          "/produtos/roupas/camiseta-masculina-de-pilates-essentials-500-domyos/amarela/camiseta-masculina-de-pilates-essentials-500-domyos-amarela1.jpg",
+          "/produtos/roupas/camiseta-masculina-de-pilates-essentials-500-domyos/amarela/camiseta-masculina-de-pilates-essentials-500-domyos-amarela2.jpg",
+          "/produtos/roupas/camiseta-masculina-de-pilates-essentials-500-domyos/amarela/camiseta-masculina-de-pilates-essentials-500-domyos-amarela3.jpg",
+          "/produtos/roupas/camiseta-masculina-de-pilates-essentials-500-domyos/amarela/camiseta-masculina-de-pilates-essentials-500-domyos-amarela4.jpg"
+        ]
+      },
+      {
+        cor: "Azul",
+        corHex: "#91BEF4",
+        imagens: [
+          "/produtos/roupas/camiseta-masculina-de-pilates-essentials-500-domyos/azul/camiseta-masculina-de-pilates-essentials-500-domyos-azul.jpg",
+          "/produtos/roupas/camiseta-masculina-de-pilates-essentials-500-domyos/azul/camiseta-masculina-de-pilates-essentials-500-domyos-azul1.jpg",
+          "/produtos/roupas/camiseta-masculina-de-pilates-essentials-500-domyos/azul/camiseta-masculina-de-pilates-essentials-500-domyos-azul2.jpg",
+          "/produtos/roupas/camiseta-masculina-de-pilates-essentials-500-domyos/azul/camiseta-masculina-de-pilates-essentials-500-domyos-azul3.jpg"
+        ]
+      },
+      {
+        cor: "Preta",
+        corHex: "#201C24",
+        imagens: [
+          "/produtos/roupas/camiseta-masculina-de-pilates-essentials-500-domyos/preta/camiseta-masculina-de-pilates-essentials-500-domyos-preta.jpg",
+          "/produtos/roupas/camiseta-masculina-de-pilates-essentials-500-domyos/preta/camiseta-masculina-de-pilates-essentials-500-domyos-preta1.jpg",
+          "/produtos/roupas/camiseta-masculina-de-pilates-essentials-500-domyos/preta/camiseta-masculina-de-pilates-essentials-500-domyos-preta2.jpg",
+          "/produtos/roupas/camiseta-masculina-de-pilates-essentials-500-domyos/preta/camiseta-masculina-de-pilates-essentials-500-domyos-preta3.jpg"
+        ]
+      }
+    ]
+  },
+  {
+    id: 3,
+    nome: "Mochila de Trilha MH500 30 Litros",
+    marca: "Quechua",
+    preco: 599.99,
+    categoria: "acessorio",
+    imagens: [
+      "/produtos/acessorios/mochilas/mh500-30L/mh500-30L.jpg",
+      "/produtos/acessorios/mochilas/mh500-30L/mh500-30L1.jpg",
+      "/produtos/acessorios/mochilas/mh500-30L/mh500-30L2.jpg",
+      "/produtos/acessorios/mochilas/mh500-30L/mh500-30L-descrition.jpg",
+    ],
+    variacoes: []
+  },
+  {
+    id: 25,
+    nome: "Mesa dobrável de 90cm Frade",
+    marca: "Nautika",
+    preco: 448.90,
+    categoria: "acessorio",
+    imagens: [
+      "/produtos/acessorios/apoio/mesa-dobravel-de-90cm-rade-nautika/mesa-dobravel-de-90cm-rade-nautika.jpg",
+      "/produtos/acessorios/apoio/mesa-dobravel-de-90cm-rade-nautika/mesa-dobravel-de-90cm-rade-nautika1.jpg",
+      "/produtos/acessorios/apoio/mesa-dobravel-de-90cm-rade-nautika/mesa-dobravel-de-90cm-rade-nautika2.jpg",
+      "/produtos/acessorios/apoio/mesa-dobravel-de-90cm-rade-nautika/mesa-dobravel-de-90cm-rade-nautika3.jpg"
+    ],
+    variacoes: []
+  },
+  {
+    id: 16,
+    nome: "Barraca de Camping Quechua 3 pessoas Mh100 Fresh & Black",
+    marca: "Quechua",
+    preco: 999.99,
+    categoria: "equipamento",
+    imagens: [
+      "/produtos/equipamentos/barracas/barraca-de-camping-quechua-3-pessoas-mh100-fresh-and-black/barraca-de-camping-quechua-3-pessoas-mh100-fresh-and-black.jpg",
+      "/produtos/equipamentos/barracas/barraca-de-camping-quechua-3-pessoas-mh100-fresh-and-black/barraca-de-camping-quechua-3-pessoas-mh100-fresh-and-black1.jpg",
+      "/produtos/equipamentos/barracas/barraca-de-camping-quechua-3-pessoas-mh100-fresh-and-black/barraca-de-camping-quechua-3-pessoas-mh100-fresh-and-black2.jpg",
+      "/produtos/equipamentos/barracas/barraca-de-camping-quechua-3-pessoas-mh100-fresh-and-black/barraca-de-camping-quechua-3-pessoas-mh100-fresh-and-black3.jpg",
+      "/produtos/equipamentos/barracas/barraca-de-camping-quechua-3-pessoas-mh100-fresh-and-black/barraca-de-camping-quechua-3-pessoas-mh100-fresh-and-black4.jpg",
+      "/produtos/equipamentos/barracas/barraca-de-camping-quechua-3-pessoas-mh100-fresh-and-black/barraca-de-camping-quechua-3-pessoas-mh100-fresh-and-black5.jpg"
+    ],
+    variacoes: []
+  },
+  {
+    id: 7, 
+    nome: "Top Feminino de Academia de Sustentação Leve",
+    marca: "Domyos",
+    preco: 89.99,
+    categoria: "roupa",
+    imagens: [
+      "/produtos/roupas/top-feminino-de-academia-de-sustentacao-leve/top-feminino-de-academia-de-sustentacao-leve.jpg",
+      "/produtos/roupas/top-feminino-de-academia-de-sustentacao-leve/top-feminino-de-academia-de-sustentacao-leve1.jpg",
+      "/produtos/roupas/top-feminino-de-academia-de-sustentacao-leve/top-feminino-de-academia-de-sustentacao-leve2.jpg",
+      "/produtos/roupas/top-feminino-de-academia-de-sustentacao-leve/top-feminino-de-academia-de-sustentacao-leve3.jpg",
+      "/produtos/roupas/top-feminino-de-academia-de-sustentacao-leve/top-feminino-de-academia-de-sustentacao-leve4.jpg",
+     ],
+    variacoes: []
+  },
+  {
+    id: 12,
+    nome: "Bota Masculina de Trilha MH500 Mid Quechua",
+    marca: "Quechua",
+    preco: 799.99,
+    categoria: "roupa",
+    imagens: [],
+    variacoes: [
+      {
+        cor: "Marrom",
+        corHex: "#95613A",
+        imagens: [
+          "/produtos/roupas/bota-masculina-de-trilha-mh500-mid-quechua/marrom/bota-masculina-de-trilha-mh500-mid-quechua-marrom.jpg",
+          "/produtos/roupas/bota-masculina-de-trilha-mh500-mid-quechua/marrom/bota-masculina-de-trilha-mh500-mid-quechua1-marrom.jpg",
+          "/produtos/roupas/bota-masculina-de-trilha-mh500-mid-quechua/marrom/bota-masculina-de-trilha-mh500-mid-quechua2-marrom.jpg",
+          "/produtos/roupas/bota-masculina-de-trilha-mh500-mid-quechua/marrom/bota-masculina-de-trilha-mh500-mid-quechua3-marrom.jpg"
+        ]
+      },
+      {
+        cor: "Cinza",
+        corHex: "#7B7A7A",
+        imagens: [
+          "/produtos/roupas/bota-masculina-de-trilha-mh500-mid-quechua/cinza/bota-masculina-de-trilha-mh500-mid-quechua-cinza.jpg",
+          "/produtos/roupas/bota-masculina-de-trilha-mh500-mid-quechua/cinza/bota-masculina-de-trilha-mh500-mid-quechua-cinza1.jpg",
+          "/produtos/roupas/bota-masculina-de-trilha-mh500-mid-quechua/cinza/bota-masculina-de-trilha-mh500-mid-quechua-cinza2.jpg",
+          "/produtos/roupas/bota-masculina-de-trilha-mh500-mid-quechua/cinza/bota-masculina-de-trilha-mh500-mid-quechua-cinza3.jpg"
+        ]
+      }
+    ]
+  },
+  {
+    id: 26, 
+    nome: "Óculos de Natação Espelhado B-Fast",
+    marca: "Nabaiji",
+    preco: 134.99,
+    categoria: "acessorio",
+    imagens: [],
+    variacoes: [
+      {
+        cor: "Azul",
+        corHex: "#2C387E",
+        imagens: [
+          "/produtos/acessorios/oculos/oculos-de-natacao-espelhado-b-fast-nabaiji/azul/oculos-de-natacao-espelhado-b-fast-nabaiji-azul.jpg",
+          "/produtos/acessorios/oculos/oculos-de-natacao-espelhado-b-fast-nabaiji/azul/oculos-de-natacao-espelhado-b-fast-nabaiji-azul1.jpg",
+          "/produtos/acessorios/oculos/oculos-de-natacao-espelhado-b-fast-nabaiji/azul/oculos-de-natacao-espelhado-b-fast-nabaiji-azul2.jpg",
+          "/produtos/acessorios/oculos/oculos-de-natacao-espelhado-b-fast-nabaiji/azul/oculos-de-natacao-espelhado-b-fast-nabaiji-azul3.jpg",
+          "/produtos/acessorios/oculos/oculos-de-natacao-espelhado-b-fast-nabaiji/azul/oculos-de-natacao-espelhado-b-fast-nabaiji-azul4.jpg",
+          "/produtos/acessorios/oculos/oculos-de-natacao-espelhado-b-fast-nabaiji/azul/oculos-de-natacao-espelhado-b-fast-nabaiji-azul5.jpg",
+        ]
+      },
+      {
+        cor: "Preto",
+        corHex: "#1E1E1E",
+        imagens: [
+          "/produtos/acessorios/oculos/oculos-de-natacao-espelhado-b-fast-nabaiji/preto/oculos-de-natacao-espelhado-b-fast-nabaiji-preto.jpg",
+          "/produtos/acessorios/oculos/oculos-de-natacao-espelhado-b-fast-nabaiji/preto/oculos-de-natacao-espelhado-b-fast-nabaiji-preto1.jpg",
+          "/produtos/acessorios/oculos/oculos-de-natacao-espelhado-b-fast-nabaiji/preto/oculos-de-natacao-espelhado-b-fast-nabaiji-preto2.jpg",
+          "/produtos/acessorios/oculos/oculos-de-natacao-espelhado-b-fast-nabaiji/preto/oculos-de-natacao-espelhado-b-fast-nabaiji-preto3.jpg",
+          "/produtos/acessorios/oculos/oculos-de-natacao-espelhado-b-fast-nabaiji/preto/oculos-de-natacao-espelhado-b-fast-nabaiji-preto4.jpg",
+        ]
+      },
+    ]
+  }
+];
+
+
+
+// banco de dados de produtos (todos os cadastrados estão aqui)
+export const produtos = [
   {
     id: 1,
     nome: "Casaco Impermeável",
@@ -631,94 +898,6 @@ export const produtosEmDestaque = [
       "/produtos/acessorios/apoio/kit-de-panelas-e-fogareiro-360-degrees-furno-stove-e-pot-set/kit-de-panelas-e-fogareiro-360-degrees-furno-stove-e-pot-set2.jpg",
       "/produtos/acessorios/apoio/kit-de-panelas-e-fogareiro-360-degrees-furno-stove-e-pot-set/kit-de-panelas-e-fogareiro-360-degrees-furno-stove-e-pot-set3.jpg",
       "/produtos/acessorios/apoio/kit-de-panelas-e-fogareiro-360-degrees-furno-stove-e-pot-set/kit-de-panelas-e-fogareiro-360-degrees-furno-stove-e-pot-set4.jpg",
-    ],
-    variacoes: []
-  },
-];
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-export const produtos = [
-  {
-    id: 1, // Adicionado
-    nome: "Casaco Impermeável",
-    marca: "Quechua",
-    preco: 89.99,
-    categoria: "roupa",
-    imagens: [
-      "/produtos/roupas/casaco/azul/casaco-azul.jpg",
-      "/produtos/roupas/casaco/azul/casaco-azul1.jpg",
-      "/produtos/roupas/casaco/azul/casaco-azul2.jpg",
-      "/produtos/roupas/casaco/azul/casaco-azul3.jpg",
-      "/produtos/roupas/casaco/azul/casaco-azul4.jpg"
-    ],
-    variacoes: [
-      {
-        cor: "Azul",
-        corHex: "#5D7A95",
-        imagens: [
-          "/produtos/roupas/casaco/azul/casaco-azul.jpg",
-          "/produtos/roupas/casaco/azul/casaco-azul1.jpg",
-          "/produtos/roupas/casaco/azul/casaco-azul2.jpg",
-          "/produtos/roupas/casaco/azul/casaco-azul3.jpg",
-          "/produtos/roupas/casaco/azul/casaco-azul4.jpg"
-        ]
-      },
-      {
-        cor: "Marinho",
-        corHex: "#232E3C",
-        imagens: [
-          "/produtos/roupas/casaco/marinho/casaco-marinho.jpg",
-          "/produtos/roupas/casaco/marinho/casaco-marinho1.jpg",
-          "/produtos/roupas/casaco/marinho/casaco-marinho2.jpg",
-          "/produtos/roupas/casaco/marinho/casaco-marinho3.jpg"
-        ]
-      }
-    ]
-  },
-  {
-    id: 2, // Adicionado
-    nome: "Barraca de Camping MH100 para 4 pessoas",
-    marca: "Quechua",
-    preco: 449.99,
-    categoria: "equipamento",
-    imagens: [
-      "/produtos/equipamentos/barracas/mh100-4p/mh100-4pessoas.jpg",
-      "/produtos/equipamentos/barracas/mh100-4p/mh100-4pessoas1.jpg",
-      "/produtos/equipamentos/barracas/mh100-4p/mh100-4pessoas2.jpg",
-      "/produtos/equipamentos/barracas/mh100-4p/mh100-4pessoas3.jpg",
-      "/produtos/equipamentos/barracas/mh100-4p/mh100-4pessoas4.jpg"
     ],
     variacoes: []
   },
